a64907240b26e37b6e7b4689d9226b78c62207e6
Mickael Noel <mickael.noel@novei.fr>
2021-12-17 10:12:11 +0100

    Fin du calendrier !!!!!!!!!!!!

--- index.php
+++ index.php
@@ -22,9 +22,8 @@ $monthList = [
 ];
 
 //définition des constantes de début et de fin pour les années
-define('START_YEAR', 1982);
+define('START_YEAR', 1980);
 define('END_YEAR', 2098);
-var_dump($_POST);
 
 //On vérifie que le formulaire a bien été selectionné
 if (isset($_POST['calendarShow'])) {
@@ -67,45 +66,91 @@ if (isset($_POST['calendarShow'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Calendrier</title>
 </head>
 
 <body>
     <form action="" method="POST" id="form">
         <label for="month">Mois</label>
         <select name="month" id="month">
-            <option value="00" disabled selected>Mois</option>
+            <option value="00" disabled <?= isset($monthSelected) ? '' : 'selected' ?>>Mois</option>
             <?php
             //Boucle pour afficher les mois
             foreach ($monthList as $monthNumber => $monthName) { ?>
-                <option value="<?= $monthNumber ?>"><?= $monthName ?></option>
+                <option value="<?= $monthNumber ?>" <?= (isset($monthSelected) && $monthSelected == $monthNumber) ? 'selected' : '' ?>><?= $monthName ?></option>
             <?php } ?>
         </select>
         <p><small><?= isset($errorList['month']) ? $errorList['month'] : '' ?></small></p>
         <label for="year">Année</label>
         <select name="year" id="year">
-            <option value="00" disabled selected>Année</option>
+            <option value="00" disabled <?= isset($yearSelected) ? '' : 'selected' ?> >Année</option>
             <?php
             //Boucle pour afficher les années
             for ($year = START_YEAR; $year <= END_YEAR; $year++) { ?>
-                <option value="<?= $year ?>"><?= $year ?></option>
+                <option value="<?= $year ?>" <?= (isset($yearSelected) && $yearSelected == $year) ? 'selected' : '' ?> ><?= $year ?></option>
             <?php } ?>
         </select>
         <p><small><?= isset($errorList['year']) ? $errorList['year'] : '' ?></small></p>
         <input type="submit" value="Afficher" name="calendarShow" id="calendarShow">
     </form>
     <!-- Si je n'ai pas d'erreur dans le formulaire, je peux afficher le calendrier -->
-<?php
-if(isset($errorList) && count($errorList) === 0){ 
-    $dayinMonth = cal_days_in_month(CAL_GREGORIAN, $monthSelected, $yearSelected);
+    <?php
+    if (isset($errorList) && count($errorList) === 0) {
+        //Nombre de jour dans le mois
+        $totalDayinMonth = cal_days_in_month(CAL_GREGORIAN, $monthSelected, $yearSelected);
+        //Premier jour du mois. 1 pour Lundi, 2 pour Mardi, etc.
+        $firstDayInMonth = date('N', mktime(0, 0, 0, $monthSelected, 1, $yearSelected));
+        $dayInMonth = 1;
     ?>
-<ul>
-    <li>Nombre de jour dans le mois de <?= $monthList[$monthSelected] ?> <?= $yearSelected ?>  : <?= $dayinMonth ?></li>
-    <li>Le premier jour du mois de <?= $monthList[$monthSelected] ?> <?= $yearSelected ?> : <?= date('N l',mktime(0,0,0,$monthSelected,1,$yearSelected)) ?></li>
-</ul>
-<?php 
-var_dump($errorList);
-} ?>
+        <p>Le premier jour du mois est : <?= $firstDayInMonth ?></p>
+        <table>
+            <thead>
+                <tr>
+                    <th>Lundi</th>
+                    <th>Mardi</th>
+                    <th>Mercredi</th>
+                    <th>Jeudi</th>
+                    <th>Vendredi</th>
+                    <th>Samedi</th>
+                    <th>Dimanche</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $nbWeek = 1;
+                $begin = microtime(true);
+                while ($dayInMonth < $totalDayinMonth) {
+                ?>
+                    <tr>
+                        <?php
+                        for ($dayInWeek = 1; $dayInWeek <= 7; $dayInWeek++) {
+                            if ($dayInMonth <= $totalDayinMonth) {
+                                //Si c'est la première semaine et que le mois n'a pas encore commencé alors j'affiche une case vide
+                                if (($dayInWeek < $firstDayInMonth) && ($nbWeek == 1)) { ?>
+                                    <td class="empty"></td>
+                                <?php } else {
+                                ?>
+                                    <td><?= $dayInMonth++ ?></td>
+                                <?php
+                                }
+                            } else { ?>
+                                <td class="empty"></td>
+                        <?php
+                            }
+                        }
+                        ?>
+                    </tr>
+                <?php
+                    $nbWeek++;
+                } 
+                $end = microtime(true);
+                ?>
+            </tbody>
+        </table>
+        <p>Cela a mis : <?= $end - $begin ?></p>
+    <?php
+    } ?>
 
 </body>
 
